75377907517100d22656a0a3e41c42803a18902e
Ivan Fanchenko <ivan.fanchenko@gmail.com>
2025-01-20 21:02:00 +0300

    second commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from Kubernetes</h1><br>";
+echo "Server IP Address is: $ip_server", "<br><p>";
+echo "Made for <font color=blue>IBA training devops course";
+?>
